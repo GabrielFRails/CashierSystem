411abe18d6ae65d837a03f022c393f0696b6a35b
GabrielFRails <gabriel.estudy.reis@gmail.com>
2023-01-16 13:21:47 -0300

    Start lib dir and begin libdata coding

---
+++ lib/libdata.py
@@ -0,0 +1,37 @@
+import psycopg2 as psg2
+import time
+
+class DatabaseBase:
+# {
+	def __init__(self):
+		self.__host = "localhost"
+		self.__user = "postgress"
+		self.__password = "postgress"
+	
+	def connect(self, database):
+		db = psg2.connect(host=self.__host, database=database, user=self.__user, password=self.__password)
+		self.postgress_instance = db
+	
+	def connect_loop(self, database=None):
+		if not database:
+			return -1
+	
+		while True:
+			try:
+				self.connect(database)
+				break
+			except Exception as e:
+				print(f"Fail to connect to {self.__user}:{self.__host}")
+				print(f"Fail reason {str(e)}")
+
+				# TODO Search for the object istance
+				# if isinstance(self.postgress_instance, ):
+				# 	del self.postgress_instance
+				
+				self.postgress_instance = None
+				time.sleep(2)
+				continue
+	
+	def db(self):
+		return self.postgress_instance
+# }
