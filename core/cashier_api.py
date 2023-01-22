1142793e194c1f20a009b3b7a0f440e84edbc7eb
GabrielFRails <gabriel.estudy.reis@gmail.com>
2023-01-22 12:50:51 -0300

    Import libapi here

--- core/cashier_api.py
+++ core/cashier_api.py
@@ -4,47 +4,12 @@
 
 from fastapi import APIRouter, FastAPI, Request, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel, Field
 from enum import Enum
+
+from lib.libapi import *
 
 app = FastAPI()
 router = APIRouter()
-
-class product(BaseModel):
-# {
-    id_product: str = Field(
-        ""
-    )
-    cod_product: str = Field(
-        ""
-    )
-    description: str = Field(
-        ""
-    )
-    unit: int = Field(
-        0
-    )
-    price: float = Field(
-        0.0
-    )
-    id_type: int = Field(
-        0
-    )
-# }
-
-class response_message(str, Enum):
-	SUCCESS: str = "SUCCESS"
-	FAIL: str = "FAIL"
-
-class default_response(BaseModel):
-	message: response_message
-
-class default_response_write_operation(default_response):
-	message: response_message = response_message.SUCCESS
-
-class default_response_error(default_response):
-	message: response_message = response_message.FAIL
-	description: str
 
 @app.get("/system_health")
 def get_system_health():
