e0fee17931c1947408ccd26b4226d649e51c76ea
GabrielFRails <gabriel.estudy.reis@gmail.com>
2023-01-22 12:48:37 -0300

    Create libapi

---
+++ lib/libapi.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from enum import Enum
+
+class response_message(str, Enum):
+	SUCCESS: str = "SUCCESS"
+	FAIL: str = "FAIL"
+
+class default_response(BaseModel):
+	message: response_message
+
+class default_response_write_operation(default_response):
+	message: response_message = response_message.SUCCESS
+
+class default_response_error(default_response):
+	message: response_message = response_message.FAIL
+	description: str
