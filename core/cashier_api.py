05cefdcec6c16a232fd6531aabebaf8fdb57c467
GabrielFRails <gabriel.estudy.reis@gmail.com>
2023-01-22 12:23:10 -0300

    Add default success and fail responses and some exemples of api endpoints

--- core/cashier_api.py
+++ core/cashier_api.py
@@ -5,6 +5,7 @@
 from fastapi import APIRouter, FastAPI, Request, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Field
+from enum import Enum
 
 app = FastAPI()
 router = APIRouter()
@@ -12,9 +13,38 @@ router = APIRouter()
 class Product(BaseModel):
 # {
     id_product: str = Field(
-        "aaa99",
+        ""
+    )
+    cod_product: str = Field(
+        ""
+    )
+    description: str = Field(
+        ""
+    )
+    unit: int = Field(
+        0
+    )
+    price: float = Field(
+        0.0
+    )
+    id_type: int = Field(
+        0
     )
 # }
+
+class response_message(str, Enum):
+	SUCCESS: str = "SUCCESS"
+	FAIL: str = "FAIL"
+
+class default_response(BaseModel):
+	message: response_message
+
+class default_response_write_operation(default_response):
+	message: response_message = response_message.SUCCESS
+
+class default_response_error(default_response):
+	message: response_message = response_message.FAIL
+	description: str
 
 @app.get("/system_health")
 def get_system_health():
@@ -25,7 +55,15 @@ def get_system_health():
 )
 def get_product(product_id: str):
 # {
-    r = Product(id_product=product_id)
+    r = Product()
 
     return r
+# }
+
+@app.put("/product", 
+    response_model=default_response_write_operation
+)
+def put_product(data: Product):
+# {
+    return { "success": True }
 # }
