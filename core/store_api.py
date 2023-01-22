39e0076980570864f6b297bd90cab8d672718a37
GabrielFRails <gabriel.estudy.reis@gmail.com>
2023-01-22 13:03:35 -0300

    Move endpoints to the correct api

--- core/store_api.py
+++ core/store_api.py
@@ -5,9 +5,29 @@
 from fastapi import APIRouter, FastAPI, Request, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
+from lib.libapi import *
+
 app = FastAPI()
 router = APIRouter()
 
 @app.get("/system_health")
 def get_system_health():
     return {"success": True}
+
+@app.get("/product",
+    response_model=product
+)
+def get_product(product_id: str):
+# {
+    r = product()
+
+    return r
+# }
+
+@app.put("/product", 
+    response_model=default_response_write_operation
+)
+def put_product(data: product):
+# {
+    return { "success": True }
+# }
