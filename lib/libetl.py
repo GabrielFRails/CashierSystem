2fe3bdc62d77c7e7686d52952c5d51941ac05b3c
GabrielFRails <gabriel.estudy.reis@gmail.com>
2023-02-05 11:39:22 -0300

    Create customer etl function

--- lib/libetl.py
+++ lib/libetl.py
@@ -1,4 +1,4 @@
-from lib.libapi import product, product_type
+from lib.libapi import product, product_type, customer
 
 def etl_product(p):
 # {
@@ -22,3 +22,14 @@ def etl_product_type(p):
 	data.description = p[1]
 
 	return data
+
+def etl_customer(c):
+# {
+	data = customer()
+
+	data.id_customer = c[0]
+	data.name = c[1]
+	data.phone = c[2]
+
+	return data
+# }
